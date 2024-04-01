b4840584679a81c185cd528ceca503cdb0c1b5fc
Alexandra Spalato <alexaspalato@gmail.com>
2024-04-01 16:24:47 +0200

    Add about.test.tsx and update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,11 @@
+import { flatRoutes } from "remix-flat-routes";
 /** @type {import('@remix-run/dev').AppConfig} */
+
 export default {
   ignoredRouteFiles: ["**/*.css"],
   tailwind: true,
   postcss: true,
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
+  routes: async (defineRoutes) => {
+    return flatRoutes("routes", defineRoutes);
+  },
 };
